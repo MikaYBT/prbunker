5f4df87a2f3872a71251188a853f9292c85508c9
MikaYBT <mystrangelife808@gmail.com>
2025-09-29 20:32:04 +0300

    fixed collapse menu

--- scripts/menu.js
+++ scripts/menu.js
@@ -2,6 +2,8 @@ menuButton = document.querySelector('.navigation__menu-button');
 collapseMenu = document.querySelector('.collapse-menu');
 closeButton = document.querySelector('.collapse-menu__close-button');
 collapseMenuOverlay = document.querySelector('.collapse-menu__overlay');
+collapseMenuLinks = document.querySelectorAll('.collapse-menu__button');
+
 
 menuButton.addEventListener('click', () => {
     collapseMenu.classList.add('collapse-menu--active');
@@ -13,4 +15,17 @@ closeButton.addEventListener('click', () => {
     collapseMenu.classList.remove('collapse-menu--active');
     collapseMenuOverlay.classList.remove('collapse-menu__overlay--active');
     }
+)
+
+collapseMenuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        collapseMenu.classList.remove('collapse-menu--active');
+        collapseMenuOverlay.classList.remove('collapse-menu__overlay--active');
+    })
+})
+
+collapseMenuOverlay.addEventListener('click', () => {
+    collapseMenu.classList.remove('collapse-menu--active');
+    collapseMenuOverlay.classList.remove('collapse-menu__overlay--active');
+}
 )

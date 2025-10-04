461ce08a58adf34ef20ae68bda181c3b5276dca8
MikaYBT <mystrangelife808@gmail.com>
2025-10-04 18:48:29 +0300

    added animations to advantages block and fixed its adaptivity

--- scripts/animations.js
+++ scripts/animations.js
@@ -46,3 +46,51 @@ function servicesAnimations(){
 
 window.addEventListener('scroll', servicesAnimations);
 window.addEventListener('resize', servicesAnimations)
+
+//advantages
+
+const map = document.querySelector(".advantages__map");
+// const advantagesBg = document.querySelector(".advantages__bg");
+const advantagesBlock = document.querySelectorAll(".advantages__card")
+const advantagesImage = document.querySelectorAll(".advantages__image")
+
+function advantagesAnimations(){
+    const windowHeight = window.innerHeight;
+    mapPosition = map.getBoundingClientRect();
+    mapDif = (mapPosition.top + mapPosition.height /2) - windowHeight /2
+    mapOpacity = 1.5 - (Math.abs(mapDif) / (windowHeight/2))
+    map.style.opacity = Math.max(0, Math.min(1, mapOpacity))
+    if (mapDif < 1){
+        map.style.clipPath = `inset(0 0 ${Math.abs(mapDif)*0.2}px 0 round 16px)`;
+    }
+
+    // bgPosition = map.getBoundingClientRect();
+    // bgDif = (bgPosition.top + bgPosition.height /2) - windowHeight /2
+    // bgTransform = 1 - (Math.abs(bgDif / windowHeight / 2))
+    // advantagesBg.style.transform = `translate(-50%, ${-50*bgTransform}%)`
+    advantagesBlock.forEach(card => {
+        blockPosition = card.getBoundingClientRect();
+        blockDif = Math.abs((blockPosition.top + blockPosition.height / 2) - windowHeight/2)
+        blockOpacity = 1 - (blockDif / windowHeight / 2)*2 + 0.1
+        card.style.opacity = Math.max(0, Math.min(1, blockOpacity))
+    })
+    advantagesImage.forEach(card => {
+        blockPosition = card.getBoundingClientRect();
+        blockDif = ((blockPosition.top + blockPosition.height / 2) - windowHeight/2)
+        blockTransform = 1 - (blockDif / windowHeight / 2)
+        if (window.innerWidth > 1024){
+            card.style.transform = `translate(0%, ${-50*blockTransform + 40}%)`
+        }
+        else if (window.innerWidth > 768){
+            card.style.transform = `translate(0%, ${-50*blockTransform}%)`
+        }
+        else {
+            card.style.transform = `translate(0%, ${-50*blockTransform + 50}%)`
+        }
+        
+    })
+
+}
+
+window.addEventListener('scroll', advantagesAnimations);
+window.addEventListener('resize', advantagesAnimations);

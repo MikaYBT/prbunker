84ff5d11efe0a9d832f9e8af0c3fbaf61cebe7c4
MikaYBT <mystrangelife808@gmail.com>
2025-10-08 19:49:09 +0300

    fixed ratio scale of hero bg

--- scripts/heroBG.js
+++ scripts/heroBG.js
@@ -3,9 +3,14 @@ const heroInfo = document.querySelector(".hero__info");
 
 function heroSize(){
     const windowHeight = window.innerHeight;
+    const windowWidth = window.innerWidth;
     heroInfoRect = heroInfo.getBoundingClientRect();
     sizeRatio = heroInfoRect.height/windowHeight;
-    heroVideo.style.height = `${(1-sizeRatio)*130}%`
+    heroVideo.style.height = `${(1-sizeRatio)*145}%`;
+    if (heroInfoRect.width < windowWidth){
+        heroVideo.classList.add("hero__video--cover");
+    }
+    else {heroVideo.classList.remove("hero__video--cover"); }
 }
 
 window.addEventListener("DOMContentLoaded", heroSize);
